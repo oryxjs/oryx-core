7fbdc4f607ace62845a630150b408499e848e84b
Assem <assemmohamed5@fci.helwan.edu.eg>
2022-08-22 23:41:32 +0200

    Update pull_or_add_branches.js

--- scripts/pull_or_add_branches.js
+++ scripts/pull_or_add_branches.js
@@ -15,49 +15,51 @@ const fs = require('fs')
 async function main() {
   git.addRemote("medusa", repoUrl)
   .then((r) => {
-    return git.raw('fetch', 'medusa')
-  }).then(() => {
-    let oryx_branches = []
+    return git.raw(['fetch', 'medusa'])
+  }).then((r) => {
+    let packages = []
     if(args.length) {
-      oryx_branches = [...args]
+      packages = [...args]
+      pull_or_add_packages(packages)
     } else {
       git.branch({
         '-r': null,
         '--list': null
       }).then((d) => {
         delete d.branches['medusa/master']
-        oryx_branches = Object.keys(d.branches).filter((b) => b.includes('medusa/')).map((b) => b.split('/')[1])
+        packages = Object.keys(d.branches).filter((b) => b.includes('medusa/')).map((b) => b.split('/')[1])
+        pull_or_add_packages(packages)
       })
     }
-    console.log(oryx_branches)
-    for(let p of oryx_branches) {
-      const shouldPull = fs.existsSync(`${process.cwd}/packages/${p}`)
-      git.raw(
-        [
-          "subtree",
-          `${shouldPull ? 'pull':'add'}`,
-          "--prefix",
-          `packages/${p}/`,
-          "medusa",
-          p,
-        ],
-        (err, resp) => {
-          if (err) {
-            git.removeRemote("medusa")
-            throw new jsGit.GitError(err)
-          }
-          console.log(
-            "Pull and Creating/Updating subtree branches done successfully \n" +
-              resp
-          )
-        }
-      )
-    }
-  }).finally(() => {
-    git.removeRemote("medusa")
   })
-  
-  
+}
+
+function pull_or_add_packages(packages) {
+  for(let p of packages) {
+    const shouldPull = fs.existsSync(`${process.cwd}/packages/${p}`)
+    git.raw(
+      [
+        "subtree",
+        `${shouldPull ? 'pull':'add'}`,
+        "--prefix",
+        `packages/${p}/`,
+        "medusa",
+        p,
+      ],
+      (err, resp) => {
+        if (err) {
+          git.removeRemote("medusa")
+          throw new jsGit.GitError(err)
+        }
+        console.log(
+          "Pull and Creating/Updating subtree branches done successfully \n" +
+            resp
+        )
+      }
+    )
+  }
+
+  git.removeRemote("medusa")
 }
 
 
