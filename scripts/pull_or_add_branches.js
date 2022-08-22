3d85a25431dbca0553b271000e29788e700208f8
Assem <assemmohamed5@fci.helwan.edu.eg>
2022-08-22 22:22:05 +0200

    refactor!: added a script to pull or add subtrees branches easily and update github actions

---
+++ scripts/pull_or_add_branches.js
@@ -0,0 +1,64 @@
+const repoUrl = "https://github.com/oryxjs/medusa.git"
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+const jsGit = require("simple-git")
+const url = new URL(repoUrl)
+const authConfig = `credential.${url.origin}.helper='!f() { echo username=${username} && echo password=${password}; }; f'`
+const git = jsGit.default({
+  config: [authConfig],
+})
+// specify some packages
+const args = process.argv.splice(2)
+const fs = require('fs')
+
+
+async function main() {
+  git.addRemote("medusa", repoUrl)
+  .then((r) => {
+    return git.raw('fetch', 'medusa')
+  }).then((d) => {
+    let oryx_branches = []
+    if(args.length) {
+      oryx_branches = [...args]
+    } else {
+      git.branch({
+        '-r': null,
+        '--list': null
+      }).then((d) => {
+        delete d.branches['medusa/master']
+        oryx_branches = Object.keys(d.branches).filter((b) => b.includes('medusa/')).map((b) => b.split('/')[1])
+      })
+    }
+    
+    for(let p of oryx_branches) {
+      const shouldPull = fs.existsSync(`${process.cwd}/packages/${p}`)
+      git.raw(
+        [
+          "subtree",
+          `${shouldPull ? 'pull':'add'}`,
+          "--prefix",
+          `packages/${p}/`,
+          "medusa",
+          p,
+        ],
+        (err, resp) => {
+          if (err) {
+            git.removeRemote("medusa")
+            throw new jsGit.GitError(err)
+          }
+          console.log(
+            "Pull and Creating/Updating subtree branches done successfully \n" +
+              resp
+          )
+        }
+      )
+    }
+
+    git.removeRemote("medusa")
+  })
+  
+  
+}
+
+
+main()
