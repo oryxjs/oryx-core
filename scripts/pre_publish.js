59d30733318925a48218f5f54d1728391a439b84
Assem <assemmohamed5@fci.helwan.edu.eg>
2022-08-23 08:55:04 +0200

    refactor!: refactor packages package.json and added a script to automate bump versions with yarn

---
+++ scripts/pre_publish.js
@@ -0,0 +1,49 @@
+const { execSync } = require('child_process')
+const fs = require('fs')
+
+function main() {
+  const packages_with_versions = {}
+  fs.readdirSync(`${process.cwd()}/packages`)
+    .filter((f) => !f.match(/file|plugin|payment|fulfillment|source|config|create/g))
+    .map((p) => {
+      try {
+        const package = require(`${process.cwd()}/packages/${p}/package.json`)
+        packages_with_versions[`${package.name}`] = package.version
+      } catch (error) {}
+    })
+
+  try {
+    const current_packages_versions = require(`${process.cwd()}/packages-versions.json`)
+    const changed_packages = diff(current_packages_versions, packages_with_versions)
+    if(!Object.keys(changed_packages).length) {
+      console.log('All packages are up to date, no bumps detected')
+    } else {
+      let bumps = ""
+      Object.keys(changed_packages)
+        .map((p) => {
+          const alias = p.replace(/oryx/g, "medusa")
+          const version = changed_packages[p]
+
+          bumps = bumps.concat(`${alias}@npm:${p}@${version} `)
+        })
+      execSync(`yarn up ${bumps}`)
+      // update new versions of packages
+      fs.writeFileSync(`${process.cwd()}/packages-versions.json`, JSON.stringify(packages_with_versions))
+    }
+  } catch (error) {
+    fs.writeFileSync(`${process.cwd()}/packages-versions.json`, JSON.stringify(packages_with_versions))
+    console.log('package-versions.json generated with all current packages versions')
+  }
+}
+
+function diff(p1, p2) {
+  const diff_pkgs = {}
+  Object.keys(p2).reduce((diff, key) => {
+    if(p1[key] === p2[key]) return diff
+
+    diff_pkgs[key] = p2[key]
+  })
+  return diff_pkgs
+}
+
+main()
